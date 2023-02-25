c1e9d7e9181893c3f1083ada10c199c22c6cc7f8
Zi <45323712+code269@users.noreply.github.com>
2023-02-25 02:28:21 -0500

    Completed 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  // Return the string multiplied by how many nums
+  if (num < 0) return "ERROR";
+  let newString = "";
+  for (let i = 0; i < num; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
