def4e5042fde97159425b8695db82badc3870f23
Zi <45323712+code269@users.noreply.github.com>
2023-02-25 20:36:13 -0500

    Completed 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  if (first < 0 || second < 0) return "ERROR";
+  if (!Number.isInteger(first) || !Number.isInteger(second)) return "ERROR";
 
+  let starter = first > second ? second : first;
+  let end = first > second ? first : second;
+  let runningSum = 0;
+  for (let i = starter; i <= end; i++) {
+    runningSum += i;
+  }
+  return runningSum;
 };
 
 // Do not edit below this line
