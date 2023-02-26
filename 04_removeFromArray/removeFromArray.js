0a461ff0a674bdc0ffc7a5a5f64ce3d361203c2c
Zi <45323712+code269@users.noreply.github.com>
2023-02-25 20:15:57 -0500

    Completed 04 remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...values) {
+  // How to match values with arr to filter out
+  return arr.filter((val) => !values.includes(val));
 };
 
 // Do not edit below this line
