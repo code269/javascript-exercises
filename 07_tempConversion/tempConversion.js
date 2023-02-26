3bffd485757c20444ef2e7c39cea7fd30f033dd1
Zi <45323712+code269@users.noreply.github.com>
2023-02-26 01:39:09 -0500

    Completed 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degrees) {
+  let fahrenheit = (degrees - 32) * 0.5556;
+  return +fahrenheit.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degrees) {
+  let celsius = degrees * 1.8 + 32;
+  return +celsius.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
