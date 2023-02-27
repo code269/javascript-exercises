b46c7843b2140a3ca01c96cd10c25f72032b94e0
Zi <45323712+code269@users.noreply.github.com>
2023-02-27 03:28:06 -0500

    Completed 12 findOldest

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (catalogue) {
+  return catalogue.map((book) => book.title);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // Get age by year
+  function getAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 
+  // Compare the ages
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+
+    return oldestAge > currentAge ? oldestPerson : currentPerson;
+  });
 };
 
 // Do not edit below this line
