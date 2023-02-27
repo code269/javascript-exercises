faaba880b143ed14349f68053a6db1fb267f24e3
Zi <45323712+code269@users.noreply.github.com>
2023-02-27 02:48:27 -0500

    Completed 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  //Remove non-alphabetical chars (spaces, commas, etc)
+  const mutatedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return mutatedString.split("").reverse().join("") === mutatedString;
 };
 
 // Do not edit below this line
